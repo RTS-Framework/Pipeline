f84d89d42ba13e8f155b13c74b36dc2d39fd9d54
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-18 16:04:33 +0800

    add isSlotTypeMatched, improve comment for InputSlot and OutputSlot.

--- slot.go
+++ slot.go
@@ -1,17 +1,36 @@
 package pipeline
 
-type InputSlot struct {
-	ID string
+import (
+	"slices"
+)
 
+// InputSlot contains the input slot information.
+type InputSlot struct {
+	// each Name in the same node must be different.
 	Name string
 
-	Accept []ArtifactType
+	// Description contains this slot information.
+	Description string
+
+	// this slot must be linked with another output slot.
+	Required bool
+
+	// defines this slot accepted artifact type.
+	Accepted []ArtifactType
 }
 
+// OutputSlot contains the output slot information.
 type OutputSlot struct {
-	ID string
-
+	// each Name in the same node must be different.
 	Name string
 
+	// Description contains this slot information.
+	Description string
+
+	// defines this slot output artifact type.
 	Type ArtifactType
+}
+
+func isSlotTypeMatched(accepted []ArtifactType, typ ArtifactType) bool {
+	return slices.Contains(accepted, typ)
 }
