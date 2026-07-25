29a295badb6d3c9548d68fc6e12ab9c19e0cb18f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-25 14:23:20 +0800

    add default logger, rewrite GetInput and GetOutput for use easily.

--- context.go
+++ context.go
@@ -3,21 +3,26 @@ package pipeline
 import (
 	"context"
 	"fmt"
+	"os"
 	"path/filepath"
 	"sync"
 	"sync/atomic"
 	"time"
 )
 
+// Context is a Pipeline execute context, it is a mirror state of Pipeline.
 type Context struct {
-	logger *logger
+	context.Context
 
-	// key is the link path
-	linkChs map[string]chan *Artifact
+	logger Logger
 
 	// key is "nodeName.slotName"
 	inputIndex  map[string]<-chan *Artifact
 	outputIndex map[string][]chan<- *Artifact
+
+	limitGetInput  map[string]struct{}
+	limitGetOutput map[string]struct{}
+	limitGetMu     sync.Mutex
 
 	// node running status
 	// key is node name
@@ -25,22 +30,25 @@ type Context struct {
 	nodesErr  map[string]error
 	nodesMu   sync.Mutex
 
-	context context.Context
-	cancel  context.CancelFunc
+	cancel context.CancelFunc
 }
 
 func (p *Pipeline) newContext(ctx context.Context, opts *Options) (*Context, error) {
-	// prepare the logger
-	logDir := opts.LogDirectory
-	if logDir == "" {
-		logDir = "logs"
-	}
-	logID := atomic.AddInt64(&p.counter, 1)
-	logName := fmt.Sprintf("pipeline-%d-%04d.log", time.Now().UnixNano(), logID)
-	logPath := filepath.Join(logDir, logName)
-	logger, err := newLogger(logPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create logger: %s", err)
+	// prepare the logger if opts.Logger is nil
+	logger := opts.Logger
+	if logger == nil {
+		logID := atomic.AddInt64(&p.counter, 1)
+		logName := fmt.Sprintf("pipeline-%d-%04d.log", time.Now().UnixNano(), logID)
+		logPath := filepath.Join("logs", logName)
+		err := os.MkdirAll("logs", 0750)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.OpenFile(logPath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600) // #nosec
+		if err != nil {
+			return nil, err
+		}
+		logger = NewLogger(file)
 	}
 	// create channel for each link
 	linkChs := make(map[string]chan *Artifact, len(p.links))
@@ -63,21 +71,26 @@ func (p *Pipeline) newContext(ctx context.Context, opts *Options) (*Context, err
 		nodesDone[name] = make(chan struct{})
 	}
 	c := &Context{
-		logger:      logger,
-		linkChs:     linkChs,
-		inputIndex:  inputIndex,
-		outputIndex: outputIndex,
-		nodesDone:   nodesDone,
-		nodesErr:    make(map[string]error),
+		logger:         logger,
+		limitGetInput:  make(map[string]struct{}),
+		limitGetOutput: make(map[string]struct{}),
+		inputIndex:     inputIndex,
+		outputIndex:    outputIndex,
+		nodesDone:      nodesDone,
+		nodesErr:       make(map[string]error),
 	}
-	c.context, c.cancel = context.WithCancel(ctx)
+	c.Context, c.cancel = context.WithCancel(ctx)
 	return c, nil
 }
 
 // GetInput is used to get input channels by Node.
 // returned map's key is the input slot name.
+// this method can only call once by each Node.
 func (ctx *Context) GetInput(node Node) map[string]<-chan *Artifact {
 	name := node.Name()
+	if ctx.alreadyGetInput(name) {
+		panic("GetInput can only be called once")
+	}
 	slots := node.Inputs()
 	channels := make(map[string]<-chan *Artifact)
 	for _, slot := range slots {
@@ -91,35 +104,71 @@ func (ctx *Context) GetInput(node Node) map[string]<-chan *Artifact {
 	return channels
 }
 
+func (ctx *Context) alreadyGetInput(name string) bool {
+	ctx.limitGetMu.Lock()
+	defer ctx.limitGetMu.Unlock()
+	if _, ok := ctx.limitGetInput[name]; ok {
+		return true
+	}
+	ctx.limitGetInput[name] = struct{}{}
+	return false
+}
+
 // GetOutput is used to get output channels by Node.
 // returned map's key is the output slot name.
-func (ctx *Context) GetOutput(node Node) map[string][]chan<- *Artifact {
+// this method can only call once by each Node.
+func (ctx *Context) GetOutput(node Node) map[string]chan<- *Artifact {
 	name := node.Name()
+	if ctx.alreadyGetOutput(name) {
+		panic("GetOutput can only be called once")
+	}
 	slots := node.Outputs()
-	channels := make(map[string][]chan<- *Artifact)
+	channels := make(map[string]chan<- *Artifact)
 	for _, slot := range slots {
 		key := name + "." + slot.Name
 		chs := ctx.outputIndex[key]
-		if chs != nil {
+		if chs == nil {
 			panic(fmt.Sprintf("invalid node implement: \"%s\"", name))
 		}
-		channels[slot.Name] = chs
+		// Node only write Artifact to channel once
+		proxy := make(chan *Artifact)
+		go func() {
+			var art *Artifact
+			select {
+			case art = <-proxy:
+			case <-ctx.Context.Done():
+				return
+			}
+			for _, ch := range chs {
+				select {
+				case ch <- art.Clone():
+				case <-ctx.Context.Done():
+					return
+				}
+			}
+		}()
+		channels[slot.Name] = proxy
 	}
 	return channels
 }
 
+func (ctx *Context) alreadyGetOutput(name string) bool {
+	ctx.limitGetMu.Lock()
+	defer ctx.limitGetMu.Unlock()
+	if _, ok := ctx.limitGetOutput[name]; ok {
+		return true
+	}
+	ctx.limitGetOutput[name] = struct{}{}
+	return false
+}
+
+// Logger is used to get logger from context, if a Node want
+// to write log, need call this method.
 func (ctx *Context) Logger() Logger {
 	return ctx.logger
 }
 
-func (ctx *Context) Done() <-chan struct{} {
-	return ctx.context.Done()
-}
-
-func (ctx *Context) Err() error {
-	return ctx.context.Err()
-}
-
+// Interrupt is used to interrupt the whole execute context.
 func (ctx *Context) Interrupt() {
 	ctx.cancel()
 }
