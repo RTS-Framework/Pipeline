a11f7e9425eaf7905aae9a9f91342d38662f54ce
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-07-25 12:57:31 +0800

    refactor the standard logger, finish the Logger interface.

--- logger.go
+++ logger.go
@@ -5,36 +5,84 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
-	"path/filepath"
+	"sync"
 )
+
+// control the logger output minimum level.
+const (
+	LevelDebug = iota
+	LevelInfo
+	LevelWarning
+	LevelError
+	LevelFatal
+	LevelDiscard = math.MaxInt8
+)
+
+// DefaultLevel is the default logger level.
+const DefaultLevel = LevelInfo
+
+// Logger is a simple logger interface, it can skip log less than level.
+// Methods about Fatal will not call panic in method, only record the
+// log from the code like "r:= recover()"
+type Logger interface {
+	Debug(v ...interface{})
+	Debugf(format string, v ...interface{})
+	Info(v ...interface{})
+	Infof(format string, v ...interface{})
+	Warning(v ...interface{})
+	Warningf(format string, v ...interface{})
+	Error(v ...interface{})
+	Errorf(format string, v ...interface{})
+	Fatal(fn string, v ...interface{})
+	Fatalf(fn, format string, v ...interface{})
+	SetLevel(level int8)
+	GetLevel() int8
+}
 
 type logger struct {
 	logger *log.Logger
-	file   *os.File
+
+	level    int8
+	levelRWM sync.RWMutex
 }
 
-func newLogger(path string) (*logger, error) {
-	if path == "" {
-		lg := log.New(os.Stdout, "", log.LstdFlags)
-		return &logger{logger: lg}, nil
+// NewLogger is used to create a standard logger.
+func NewLogger(w io.Writer) Logger {
+	if w == nil {
+		w = os.Stdout
+	} else {
+		w = io.MultiWriter(os.Stdout, w)
 	}
-	dir := filepath.Dir(path)
-	if dir != "." {
-		err := os.MkdirAll(dir, 0750)
-		if err != nil {
-			return nil, err
-		}
+	lg := log.New(w, "", log.LstdFlags)
+	return &logger{logger: lg, level: DefaultLevel}
+}
+
+func (l *logger) Debug(v ...interface{}) {
+	if l.GetLevel() > LevelDebug {
+		return
 	}
-	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0600) // #nosec
-	if err != nil {
-		return nil, err
+	buf := bytes.NewBuffer(make([]byte, 0, 64))
+	buf.WriteString("[debug] ")
+	_, _ = fmt.Fprintln(buf, v...)
+	l.logger.Print(buf)
+}
+
+func (l *logger) Debugf(format string, v ...interface{}) {
+	if l.GetLevel() > LevelDebug {
+		return
 	}
-	lg := log.New(io.MultiWriter(os.Stdout, file), "", log.LstdFlags)
-	return &logger{logger: lg, file: file}, nil
+	buf := bytes.NewBuffer(make([]byte, 0, 64))
+	buf.WriteString("[debug] ")
+	_, _ = fmt.Fprintf(buf, format, v...)
+	l.logger.Println(buf)
 }
 
 func (l *logger) Info(v ...interface{}) {
+	if l.GetLevel() > LevelInfo {
+		return
+	}
 	buf := bytes.NewBuffer(make([]byte, 0, 64))
 	buf.WriteString("[info] ")
 	_, _ = fmt.Fprintln(buf, v...)
@@ -42,6 +90,9 @@ func (l *logger) Info(v ...interface{}) {
 }
 
 func (l *logger) Infof(format string, v ...interface{}) {
+	if l.GetLevel() > LevelInfo {
+		return
+	}
 	buf := bytes.NewBuffer(make([]byte, 0, 64))
 	buf.WriteString("[info] ")
 	_, _ = fmt.Fprintf(buf, format, v...)
@@ -49,6 +100,9 @@ func (l *logger) Infof(format string, v ...interface{}) {
 }
 
 func (l *logger) Warning(v ...interface{}) {
+	if l.GetLevel() > LevelWarning {
+		return
+	}
 	buf := bytes.NewBuffer(make([]byte, 0, 64))
 	buf.WriteString("[warning] ")
 	_, _ = fmt.Fprintln(buf, v...)
@@ -56,6 +110,9 @@ func (l *logger) Warning(v ...interface{}) {
 }
 
 func (l *logger) Warningf(format string, v ...interface{}) {
+	if l.GetLevel() > LevelWarning {
+		return
+	}
 	buf := bytes.NewBuffer(make([]byte, 0, 64))
 	buf.WriteString("[warning] ")
 	_, _ = fmt.Fprintf(buf, format, v...)
@@ -63,6 +120,9 @@ func (l *logger) Warningf(format string, v ...interface{}) {
 }
 
 func (l *logger) Error(v ...interface{}) {
+	if l.GetLevel() > LevelError {
+		return
+	}
 	buf := bytes.NewBuffer(make([]byte, 0, 64))
 	buf.WriteString("[error] ")
 	_, _ = fmt.Fprintln(buf, v...)
@@ -70,6 +130,9 @@ func (l *logger) Error(v ...interface{}) {
 }
 
 func (l *logger) Errorf(format string, v ...interface{}) {
+	if l.GetLevel() > LevelError {
+		return
+	}
 	buf := bytes.NewBuffer(make([]byte, 0, 64))
 	buf.WriteString("[error] ")
 	_, _ = fmt.Fprintf(buf, format, v...)
@@ -77,6 +140,9 @@ func (l *logger) Errorf(format string, v ...interface{}) {
 }
 
 func (l *logger) Fatal(fn string, v ...interface{}) {
+	if l.GetLevel() > LevelFatal {
+		return
+	}
 	buf := bytes.NewBuffer(make([]byte, 0, 64))
 	buf.WriteString("[fatal] ")
 	buf.WriteString("panic in " + fn + "\n")
@@ -85,6 +151,9 @@ func (l *logger) Fatal(fn string, v ...interface{}) {
 }
 
 func (l *logger) Fatalf(fn, format string, v ...interface{}) {
+	if l.GetLevel() > LevelFatal {
+		return
+	}
 	buf := bytes.NewBuffer(make([]byte, 0, 64))
 	buf.WriteString("[fatal] ")
 	buf.WriteString("panic in " + fn + "\n")
@@ -92,15 +161,14 @@ func (l *logger) Fatalf(fn, format string, v ...interface{}) {
 	l.logger.Println(buf)
 }
 
-func (l *logger) Close() error {
-	var err error
-	if l.file != nil {
-		err = l.file.Close()
-		if err != nil {
-			l.logger.SetOutput(os.Stderr)
-			l.Error("failed to close log file:", err)
-		}
-	}
-	l.logger.SetOutput(io.Discard)
-	return err
+func (l *logger) SetLevel(level int8) {
+	l.levelRWM.Lock()
+	defer l.levelRWM.Unlock()
+	l.level = level
+}
+
+func (l *logger) GetLevel() int8 {
+	l.levelRWM.RLock()
+	defer l.levelRWM.RUnlock()
+	return l.level
 }
