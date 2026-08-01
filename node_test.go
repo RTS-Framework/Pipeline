afa19eb97f3fb2f25b806f0d020a522c3125b4d8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-01 15:05:36 +0800

    add testNode for build the test node graph, add testNewTestNode.

--- node_test.go
+++ node_test.go
@@ -1,1 +1,50 @@
 package pipeline
+
+type testNode struct {
+	name string
+
+	inputs  []*InputSlot
+	outputs []*OutputSlot
+
+	exec func(ctx *Context) error
+}
+
+func testNewTestNode(name string) *testNode {
+	return &testNode{name: name}
+}
+
+func (n *testNode) Name() string {
+	return n.name
+}
+
+func (n *testNode) Type() string {
+	return "test"
+}
+
+func (n *testNode) Description() string {
+	return "test node"
+}
+
+func (n *testNode) BuildTag() string {
+	return ""
+}
+
+func (n *testNode) Inputs() []*InputSlot {
+	return n.inputs
+}
+
+func (n *testNode) Outputs() []*OutputSlot {
+	return n.outputs
+}
+
+func (n *testNode) Initialize() error {
+	return nil
+}
+
+func (n *testNode) Execute(ctx *Context) error {
+	return n.exec(ctx)
+}
+
+func (n *testNode) Close() error {
+	return nil
+}
