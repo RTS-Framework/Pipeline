edb4ea9ce3d2807705a6d6c8c0e0ac4656d020ef
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-15 14:48:52 +0800

    add three test artifact types for context, node and pipeline.

--- artifact_test.go
+++ artifact_test.go
@@ -1,1 +1,18 @@
 package pipeline
+
+var (
+	testTypeA = ArtifactType{
+		Name:        "TestTypeA",
+		Description: "type A for test",
+	}
+
+	testTypeB = ArtifactType{
+		Name:        "TestTypeB",
+		Description: "type B for test",
+	}
+
+	testTypeC = ArtifactType{
+		Name:        "TestTypeC",
+		Description: "type C for test",
+	}
+)
