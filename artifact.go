bf2a8f86f79397b2aac0ec1e3315b2a5b9ab07ff
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-28 15:13:32 +0800

    improve RegisterArtifactType that will record the reflect.Type for CheckArtifactData.

--- artifact.go
+++ artifact.go
@@ -1,7 +1,9 @@
 package pipeline
 
 import (
+	"errors"
 	"fmt"
+	"reflect"
 	"strings"
 	"sync"
 )
@@ -21,29 +23,42 @@ func (art *Artifact) Clone() *Artifact {
 
 // ArtifactType defines the artifact type information.
 type ArtifactType struct {
-	Name        string
-	Description string
+	Name        string `toml:"name"        json:"name"`
+	Description string `toml:"description" json:"description"`
 }
 
 var (
-	artifactTypes   map[string]ArtifactType
-	artifactTypesMu sync.Mutex
+	artifactTypes    map[string]reflect.Type
+	artifactTypesRWM sync.RWMutex
 )
 
 func init() {
-	artifactTypes = make(map[string]ArtifactType, 64)
+	artifactTypes = make(map[string]reflect.Type, 64)
 }
 
-// RegisterArtifactTypes is used to register the artifact type.
-func RegisterArtifactTypes(types ...ArtifactType) error {
-	artifactTypesMu.Lock()
-	defer artifactTypesMu.Unlock()
-	for _, typ := range types {
-		key := strings.ToLower(typ.Name)
-		if _, ok := artifactTypes[key]; ok {
-			return fmt.Errorf("artifact type %s already exists", typ.Name)
-		}
-		artifactTypes[key] = typ
+// RegisterArtifactType is used to register the artifact type.
+func RegisterArtifactType(typ ArtifactType, reference any) error {
+	artifactTypesRWM.Lock()
+	defer artifactTypesRWM.Unlock()
+	key := strings.ToLower(typ.Name)
+	if _, ok := artifactTypes[key]; ok {
+		return fmt.Errorf("artifact type %s already exists", typ.Name)
+	}
+	artifactTypes[key] = reflect.TypeOf(reference)
+	return nil
+}
+
+// CheckArtifactData is used to check artifact data type is expected.
+func CheckArtifactData(name string, data any) error {
+	artifactTypesRWM.RLock()
+	defer artifactTypesRWM.RUnlock()
+	key := strings.ToLower(name)
+	typ, ok := artifactTypes[key]
+	if !ok {
+		return fmt.Errorf("artifact type %s does not exist", name)
+	}
+	if reflect.TypeOf(data) != typ {
+		return errors.New("unexpected artifact data type")
 	}
 	return nil
 }
