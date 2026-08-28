d7e1e87f664a9ce4cfe11fe8e28d9dd4f60fced3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-08-28 15:14:17 +0800

    add TestRegisterArtifactType and TestCheckArtifactData, register test types.

--- artifact_test.go
+++ artifact_test.go
@@ -1,5 +1,11 @@
 package pipeline
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
 var (
 	testTypeA = ArtifactType{
 		Name:        "TestTypeA",
@@ -16,3 +22,42 @@ var (
 		Description: "type C for test",
 	}
 )
+
+func init() {
+	err := RegisterArtifactType(testTypeA, int8(0))
+	if err != nil {
+		panic(err)
+	}
+	err = RegisterArtifactType(testTypeB, int16(0))
+	if err != nil {
+		panic(err)
+	}
+	err = RegisterArtifactType(testTypeC, int32(0))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestRegisterArtifactType(t *testing.T) {
+	t.Run("already exists", func(t *testing.T) {
+		err := RegisterArtifactType(testTypeA, int8(0))
+		require.EqualError(t, err, "artifact type TestTypeA already exists")
+	})
+}
+
+func TestCheckArtifactData(t *testing.T) {
+	t.Run("expected", func(t *testing.T) {
+		err := CheckArtifactData(testTypeA.Name, int8(123))
+		require.NoError(t, err)
+	})
+
+	t.Run("unexpected", func(t *testing.T) {
+		err := CheckArtifactData(testTypeA.Name, int16(456))
+		require.EqualError(t, err, "unexpected artifact data type")
+	})
+
+	t.Run("unknown type", func(t *testing.T) {
+		err := CheckArtifactData("unknown", 789)
+		require.EqualError(t, err, "artifact type unknown does not exist")
+	})
+}
