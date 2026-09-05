ff52665692f519cff3789d80444baf66541025b3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-05 13:02:25 +0800

    improve MatchBuildTag about empty build tag, add CheckNodeBuildTag.

--- constraint.go
+++ constraint.go
@@ -1,12 +1,17 @@
 package pipeline
 
 import (
+	"fmt"
 	"go/build/constraint"
 	"runtime"
 	"strings"
 )
 
-func matchBuildConstraint(line string) (bool, error) {
+// MatchBuildTag is used to check build tag is match current environment.
+func MatchBuildTag(line string) (bool, error) {
+	if line == "" {
+		return true, nil
+	}
 	if !strings.HasPrefix(line, "//go:build ") {
 		line = "//go:build " + line
 	}
@@ -23,4 +28,18 @@ func matchBuildTag(tag string) bool {
 		return true
 	}
 	return false
+}
+
+// CheckNodeBuildTag is used to check build tag is match current environment.
+func CheckNodeBuildTag(node Node) error {
+	buildTag := node.BuildTag()
+	ok, err := MatchBuildTag(buildTag)
+	if err != nil {
+		return err
+	}
+	if !ok {
+		format := "build tag: \"%s\" is not match current environment"
+		return fmt.Errorf(format, buildTag)
+	}
+	return nil
 }
