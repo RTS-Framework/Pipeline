1ca2e156c8070f57e8037cd9a143657c24a96e8b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-05 13:06:18 +0800

    update TestMatchBuildTag about testdata, add TestCheckNodeBuildTag.

--- constraint_test.go
+++ constraint_test.go
@@ -7,17 +7,46 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMatchBuildConstraint(t *testing.T) {
-	constraints := []string{
+func TestMatchBuildTag(t *testing.T) {
+	tags := []string{
+		"",
 		"windows && amd64",
 		"linux && amd64",
 		"windows || linux",
 		"!linux",
 	}
 
-	for _, c := range constraints {
-		ok, err := matchBuildConstraint(c)
+	for _, tag := range tags {
+		ok, err := MatchBuildTag(tag)
 		require.NoError(t, err)
-		fmt.Printf("%-16s -> %t\n", c, ok)
+
+		fmt.Printf("%-16s -> %t\n", tag, ok)
 	}
+}
+
+func TestCheckNodeBuildTag(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
+		node := testNewTestNode("test")
+
+		err := CheckNodeBuildTag(node)
+		require.NoError(t, err)
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		node := testNewTestNode("test")
+		node.buildTag = "+invalid"
+
+		err := CheckNodeBuildTag(node)
+		errStr := "invalid syntax at +"
+		require.EqualError(t, err, errStr)
+	})
+
+	t.Run("not matched", func(t *testing.T) {
+		node := testNewTestNode("test")
+		node.buildTag = "tag && cgo"
+
+		err := CheckNodeBuildTag(node)
+		errStr := "build tag: \"tag && cgo\" is not match current environment"
+		require.EqualError(t, err, errStr)
+	})
 }
