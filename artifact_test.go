3a75ed7fa3b3420a5570ed7e18d62fb6a52301f5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-12 13:30:28 +0800

    add more tests to TestRegisterArtifactType and TestCheckArtifactData.

--- artifact_test.go
+++ artifact_test.go
@@ -43,21 +43,62 @@ func TestRegisterArtifactType(t *testing.T) {
 		err := RegisterArtifactType(testTypeA, int8(0))
 		require.EqualError(t, err, "artifact type TestTypeA already exists")
 	})
+
+	t.Run("empty type name", func(t *testing.T) {
+		empty := ArtifactType{}
+
+		err := RegisterArtifactType(empty, int8(0))
+		require.EqualError(t, err, "artifact type name can not be empty")
+	})
+
+	t.Run("nil reference", func(t *testing.T) {
+		err := RegisterArtifactType(testTypeA, nil)
+		require.EqualError(t, err, "artifact reference can not be nil")
+	})
+}
+
+func TestLookupArtifactType(t *testing.T) {
+	t.Run("exist", func(t *testing.T) {
+		ok := LookupArtifactType(testTypeA)
+		require.True(t, ok)
+	})
+
+	t.Run("not exist", func(t *testing.T) {
+		unknown := ArtifactType{
+			Name: "unknown",
+		}
+
+		ok := LookupArtifactType(unknown)
+		require.False(t, ok)
+	})
+
+	t.Run("case-insensitive", func(t *testing.T) {
+		lower := ArtifactType{
+			Name: "testtypea",
+		}
+
+		ok := LookupArtifactType(lower)
+		require.True(t, ok)
+	})
 }
 
 func TestCheckArtifactData(t *testing.T) {
 	t.Run("expected", func(t *testing.T) {
-		err := CheckArtifactData(testTypeA.Name, int8(123))
+		err := CheckArtifactData(testTypeA, int8(123))
 		require.NoError(t, err)
 	})
 
 	t.Run("unexpected", func(t *testing.T) {
-		err := CheckArtifactData(testTypeA.Name, int16(456))
+		err := CheckArtifactData(testTypeA, int16(456))
 		require.EqualError(t, err, "unexpected artifact data type")
 	})
 
 	t.Run("unknown type", func(t *testing.T) {
-		err := CheckArtifactData("unknown", 789)
+		unknown := ArtifactType{
+			Name: "unknown",
+		}
+
+		err := CheckArtifactData(unknown, 789)
 		require.EqualError(t, err, "artifact type unknown does not exist")
 	})
 }
